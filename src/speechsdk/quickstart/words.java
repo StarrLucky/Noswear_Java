4110c3c5be91a543141d1b185a4a521964892041
StarrLucky <fxndstrs@gmail.com>
2020-09-29 20:37:26 +0300

    Update words.java

--- src/speechsdk/quickstart/words.java
+++ src/speechsdk/quickstart/words.java
@@ -12,87 +12,20 @@ public class words {
     private int foundInTotal = 0;
     private  String[] profanity_dic = new String[0];
     private  List<String> prof_dic = new ArrayList<>();
-    private String[] swr  = new String[] {
-            "привет",
-            "гандон",
-            "сука",
-            "суки",
-            "ссанина",
-            "блять",
-            "блять",
-            "хуй",
-            "пидор",
-            "пидорасы",
-            "пидарас",
-            "пидорасина",
-            "пидорские",
-            "дахули",
-            "бляяя",
-            "бляяять",
-            "пиздец",
-            "мразь",
-            "сучара",
-            "жмых",
-            "жмыхи",
-            "ебаный",
-            "ебаные",
-            "бляди",
-            "заебали",
-            "заебал",
-            "заебало",
-            "заебала",
-            "говно",
-            "говнище",
-            "ебаное",
-            "ебаный",
-            "ебаный",
-            "ебаная",
-            "ебанись",
-            "ебал",
-            "ебашить",
-            "ебать"
-
-    };
 
     public void loadProfanityDictionary() {
     String str = null;
+    
         try (BufferedReader reader = new BufferedReader(new FileReader("E:\\java2\\microsoft_speech\\jre\\from-microphone\\target\\dict\\profanity_dic.txt")))  {
-
-            while((str = reader.readLine())!=null) {
+            while((str = reader.readLine())!=null)
+            {
             prof_dic.add(str);
             }
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-      //  System.out.println("LIST:");
-        //   System.out.println(prof_dic.toString());
-
     }
-
-
-    public int checkForBadWords(String str)
-    {
-
-        for (int i=1; i< swr.length; i++)
-        {
-            if (str.toLowerCase().contains(swr[i])) {
-                fnd++;
-                foundInTotal++;
-                System.out.println("Found:" + swr[i] + " Total:" + foundInTotal);
-            }
-
-        }
-
-        if (fnd>0)
-        {
-            fnd = 0;
-            return 1;
-        }
-        fnd = 0;
-        return 0;
-    }
-
+    
 
     public int checkForBadWords2(String str)
     {
@@ -109,7 +42,8 @@ public class words {
                 if (prof_dic.contains(words[i].toLowerCase())){
                     fnd++;
                     foundInTotal++;
-                    System.out.println("Found: " + words[i].toLowerCase() + "Total: " + foundInTotal);
+                    System.out.print("(Распознано: " + words[i].toLowerCase() +")\n");
+                    // System.out.println("Распознано: " + words[i].toLowerCase() + " Всего: " + foundInTotal);
                 }
             }
         }
@@ -118,15 +52,13 @@ public class words {
             if (prof_dic.contains(words[0].toLowerCase())) {
                 fnd++;
                 foundInTotal++;
-                System.out.println("Found: " + words[0].toLowerCase() + "Total: " + foundInTotal);
+                System.out.print("(Распознано: " + words[0].toLowerCase() + ")\n");
             }
-            }
-
-        else {
-            System.out.println(" Error with checking word in dictionary ");
+            } 
+        else { 
+            System.out.println(" Error with checking word in dictionary "); 
         }
-
-
+        
         if (fnd>0)
         {
             return 1;
@@ -135,9 +67,5 @@ public class words {
 
         return 0;
     }
-
-
 }
-
-
 
